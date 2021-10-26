3c7d29b6de1c8641ffa6cfa4ce441c476cbcfe85
konyatomi <konyatomi16@gmail.com>
2021-10-26 12:59:45 +0200

    Completes Issue #1

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 //TODO: Randomize class, Sort class, BubbleSort class
+
+class Stack
+{
+    constructor()
+    {
+        this._stackArray = [];
+    }
+    
+    push(item)
+    {
+        this._stackArray.push(item);
+    }
+    
+    pop()
+    {
+        return this._stackArray.pop();
+    }
+
+    get size()
+    {
+        return this._stackArray.length;
+    }
+}
